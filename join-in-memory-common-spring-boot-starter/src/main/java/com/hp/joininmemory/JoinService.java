4c5b87f528ca08b4d864608db221fa0cdbcb4244
hupeng <hup_dev@outlook.com>
2024-03-29 17:03:18 +0800

    #5 fixed
    It's a common IF mistake.
    
    Signed-off-by: hupeng <hup_dev@outlook.com>

--- join-in-memory-common-spring-boot-starter/src/main/java/com/hp/joininmemory/JoinService.java
+++ join-in-memory-common-spring-boot-starter/src/main/java/com/hp/joininmemory/JoinService.java
@@ -12,24 +12,25 @@ import java.util.Iterator;
 public interface JoinService {
 
     @SuppressWarnings("unchecked")
-    default <T> void joinInMemory(T t){
-        if (t == null){
+    default <T> void joinInMemory(T t) {
+        if (t == null) {
             return;
         }
         joinInMemory((Class<T>) t.getClass(), Collections.singletonList(t));
     }
 
     @SuppressWarnings("unchecked")
-    default <T> void joinInMemory(Collection<T> t){
-        if (CollUtil.isEmpty(t)){
+    default <T> void joinInMemory(Collection<T> t) {
+        if (CollUtil.isEmpty(t)) {
             return;
         }
         final Iterator<T> iterator = t.iterator();
         final T next = iterator.next();
-        if (t.size() == 1){
+        if (t.size() == 1) {
             joinInMemory(next);
+        } else {
+            joinInMemory((Class<T>) next.getClass(), t);
         }
-        joinInMemory((Class<T>) next.getClass(), t);
     }
 
     <T> void joinInMemory(Class<T> tCls, Collection<T> t);
